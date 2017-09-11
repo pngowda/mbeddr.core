0bb1ac48edcc9eda16da3e49253dbf97a283d176
Prajwal <pgowda@itemis.de>
2017-09-11 10:06:21 +0200

    incremental changes

--- buildSrc/src/main/groovy/ExecuteMPSBuildLanguages.groovy
+++ buildSrc/src/main/groovy/ExecuteMPSBuildLanguages.groovy
@@ -1,13 +1,5 @@
 class ExecuteMPSBuildLanguages extends ExecuteMPSGeneratedAntScript {
     ExecuteMPSBuildLanguages() {
         targets 'clean', 'generate', 'assemble'
-        /*if (project.hasProperty('inc')){
-            println "Incremental mode is enabled"
-            targets 'generate', 'assemble'
-        } else {
-            println "Incremental mode is disabled"
-            targets 'clean', 'generate', 'assemble'
-        }*/
-
     }
 }
--- buildSrc/src/main/groovy/ExecuteMPSTestLanguages.groovy
+++ buildSrc/src/main/groovy/ExecuteMPSTestLanguages.groovy
@@ -1,12 +1,5 @@
 class ExecuteMPSTestLanguages extends ExecuteMPSGeneratedAntScript {
     ExecuteMPSTestLanguages() {
         targets 'clean', 'generate', 'assemble', 'check'
-        /*if (project.hasProperty('inc')){
-            println "Incremental mode is enabled"
-            targets 'generate', 'assemble', 'check'
-        } else {
-            println "Incremental mode is disabled"
-           targets 'clean', 'generate', 'assemble', 'check'
-        }*/
     }
 }
